84614d31f29dd47900337145ad4eec993bd24052
Juandiazcl <juanfcodiazvillegas@gmail.com>
2022-04-22 00:57:41 -0400

    Version ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
-$(window).scroll(function(){
-
-    if($('menuc').offset().top>400){
-        $('menuc').removeclass("")  
-    }
-})
+// funcion padre que asegura primero se carguen la página
+$(document).ready(function () {
+    // funciones hijas
+    $(".nav-link").on('click', function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html').animate({
+          scrollTop: $(hash).offset().top
+        },
+          1600, function () {
+            window.location.hash = hash;
+          });
+      }
+    });
+  })
+  
+  // asegurando funcionalidad
+  // poppover
+  // var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  // var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  //   return new bootstrap.Popover(popoverTriggerEl)
+  // })
+  // tooltip
+  var tooltipTriggerList =
+    [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
